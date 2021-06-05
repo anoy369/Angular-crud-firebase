4098addf185a039895bbd8cf119d28c565cb31ee
Amit Ghosh <amit.ghosh@cefalo.com>
2021-06-06 03:51:44 +0600

    Code refactor

--- src/app/sevices/crud.service.ts
+++ src/app/sevices/crud.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Employee } from '../interfaces/employee';
-import { AngularFireDatabaseModule } from '@angular/fire/database'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor(public fireservice: AngularFirestore, private firedatabase: AngularFireDatabaseModule) { }
+  constructor(public fireservice: AngularFirestore) { }
 
   createEmployee(employee: Employee): any{
     return this.fireservice.collection('Employee').add(employee);
